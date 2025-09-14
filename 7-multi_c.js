dff54abf14afca6350dfa1e7dc2d76085c598331
Agoro-adetola <buzzartconcept@gmail.com>
2025-09-14 08:22:40 +0100

    Update script to print 'C is fun' multiple times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,12 @@
+// Get the first command-line argument and convert it to an integer
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
-  console.log("Missing number of occurrences");
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log("C is fun");
-  }
+// Check if the argument is missing or invalid
+if (process.argv[2] === undefined || isNaN(x)) {
+    console.log("Missing number of occurrences");
+} else if (x > 0) {
+    // Use a for loop to print "C is fun" x times
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
 }
